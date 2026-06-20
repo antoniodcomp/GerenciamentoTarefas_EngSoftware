6c04f720aa37e0a099902ac35e770bec9e9d60a5
antoniodcomp <antonio.reis@dcomp.ufs.br>
2026-06-20 18:36:41 -0300

    Modelagem inicial do banco de dados

---
+++ backend/users/models.py
@@ -0,0 +1,28 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class Usuario(AbstractUser):
+    ADMINISTRADOR = 'ADMINISTRADOR'
+    GESTOR = 'GESTOR'
+    COMUM = 'COMUM'
+    TIPO_CHOICES = [
+        (ADMINISTRADOR, 'Administrador'),
+        (GESTOR, 'Gestor'),
+        (COMUM, 'Comum'),
+    ]
+
+    
+    
+    
+    nome = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    
+    
+    cargoProfissional = models.CharField(max_length=255)
+    tipo = models.CharField(max_length=20, choices=TIPO_CHOICES, default=COMUM)
+
+    class Meta:
+        db_table = 'Usuario'
+
+    def __str__(self):
+        return f"{self.nome} ({self.email})"
