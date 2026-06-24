55786b13266f7aaeda0cffad5c2f37d7ab34ef30
lucasdv120105 <164914244+lucasdv120105@users.noreply.github.com>
2026-06-24 17:32:24 -0300

    feat: adiciona rota de registro de usuario #5

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import hello_backend
+from .views import hello_backend, UserRegistrationView
 
 urlpatterns = [
     path('hello/', hello_backend, name='hello_backend'),
+    path('registro/', UserRegistrationView.as_view(), name='user_registration'),
 ]
