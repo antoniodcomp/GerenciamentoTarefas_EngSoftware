ccb6f55250cc629cd8b09188df5fd97e98b03a23
lucasdv120105 <164914244+lucasdv120105@users.noreply.github.com>
2026-06-24 17:29:58 -0300

    feat: implementa view de registro de usuario #5

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,7 +1,14 @@
 from django.http import JsonResponse
+from rest_framework.generics import CreateAPIView
+from rest_framework.permissions import AllowAny
+from .serializers import UserRegistrationSerializer
 
 def hello_backend(request):
     return JsonResponse({
         "status": "Backend online", 
         "mensagem": "Infraestrutura inicial do Django criada com sucesso!"
     })
+
+class UserRegistrationView(CreateAPIView):
+    serializer_class = UserRegistrationSerializer
+    permission_classes = [AllowAny]
