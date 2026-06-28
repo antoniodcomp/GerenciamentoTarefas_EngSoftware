2eb1c4fbc1b69a741df4d3302bf5811dc97cb60c
lucasdv120105 <lucasdv1215@academico.ufs.br>
2026-06-28 15:28:43 -0300

    feat: adiciona rotas de login e refresh token #8

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -4,4 +4,6 @@ from .views import hello_backend, UserRegistrationView
 urlpatterns = [
     path('hello/', hello_backend, name='hello_backend'),
     path('registro/', UserRegistrationView.as_view(), name='user_registration'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('login/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
