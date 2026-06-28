764e29563f4047eea2adaed0c295cd5ec1fe530e
lucasdv120105 <lucasdv1215@academico.ufs.br>
2026-06-28 16:01:27 -0300

    feat: importa e configura as views de login e refresh do SimpleJWT #8

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
 from .views import hello_backend, UserRegistrationView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView as LoginView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path('hello/', hello_backend, name='hello_backend'),
