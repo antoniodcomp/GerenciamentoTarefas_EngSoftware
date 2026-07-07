b99a25b26004e68757e37405a17d1a99a05ed636
lucasdv120105 <lucasdv1215@academico.ufs.br>
2026-07-06 21:08:54 -0300

    Correção de imports a mais no código #39

--- backend/users/views.py
+++ backend/users/views.py
@@ -4,11 +4,6 @@ from django.conf import settings
 from rest_framework.generics import CreateAPIView
 from rest_framework.permissions import AllowAny
 from rest_framework.permissions import IsAuthenticated
-from .serializers import (
-    UserRegistrationSerializer, CustomTokenObtainPairSerializer,
-    UserProfileSerializer, ChangePasswordSerializer,
-    UserListSerializer, UserTipoUpdateSerializer
-)
 from rest_framework.views import APIView                                                                                                                          
 from rest_framework.response import Response                                                                                                                      
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
